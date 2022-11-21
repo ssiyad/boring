0a2d3bba1a425598daa0b2df84273c37da84aea2
Sabu Siyad <hello@ssiyad.com>
2022-11-21 19:56:26 +0530

    feat: back to top
    Signed-off-by: Sabu Siyad <hello@ssiyad.com>

--- static/script.js
+++ static/script.js
@@ -2,7 +2,23 @@ function toggleDarkMode() {
     document.body.classList.toggle('dark');
 }
 
+function toggleBackToTop() {
+    const e = document.getElementById('back-to-top');
+    if (window.scrollY === 0) e.classList.add('hidden');
+    else e.classList.remove('hidden');
+}
+
+function backToTop() {
+    window.scrollTo(0, 0);
+}
+
 document
     .getElementById('darkmode-toggle')
     .addEventListener('click', toggleDarkMode);
+
+document
+    .getElementById('back-to-top')
+    .addEventListener('click', backToTop);
+
+window.addEventListener('scroll', toggleBackToTop)
 
