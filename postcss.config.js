ccefa83c01fb32d299f8f273948207fcde366512
Sabu Siyad <hello@ssiyad.com>
2022-11-23 21:59:29 +0530

    feat: cssnano
    Signed-off-by: Sabu Siyad <hello@ssiyad.com>

--- postcss.config.js
+++ postcss.config.js
@@ -4,5 +4,8 @@ module.exports = {
         require('tailwindcss/nesting'),
         require('tailwindcss'),
         require('autoprefixer'),
+        require('cssnano')({
+            preset: 'default',
+        }),
     ]
 }
