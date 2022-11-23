f099f6a9a655f34640cd149d53584bb6e54be277
Sabu Siyad <hello@ssiyad.com>
2022-11-23 20:08:25 +0530

    feat: fonts
    Signed-off-by: Sabu Siyad <hello@ssiyad.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./templates/**/*.html", "./theme/**/*.html"],
   darkMode: 'class',
-  theme: {},
+  theme: {
+    fontFamily: {
+      serif: ['"Crimson Pro"', ...defaultTheme.fontFamily.serif],
+      sans: ['"Work Sans"', ...defaultTheme.fontFamily.sans],
+      mono: ['"JetBrains Mono"', ...defaultTheme.fontFamily.mono],
+    }
+  },
   variants: {},
   plugins: [
     require('@tailwindcss/typography'),
