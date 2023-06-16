283f250f7e70b98b7968ced0f3ff33b2a24ad071
Sabu Siyad <hello@ssiyad.com>
2023-06-16 21:38:26 +0530

    fix: container size

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,18 @@ module.exports = {
   content: ["./templates/**/*.html", "./theme/**/*.html"],
   darkMode: 'class',
   theme: {
+    container: {
+      center: true,
+      screens: {
+        lg: '800px',
+        xl: '900px',
+        '2xl': '900px',
+      },
+    },
     fontFamily: {
       serif: ['"Crimson Pro"', ...defaultTheme.fontFamily.serif],
       sans: ['"Work Sans"', ...defaultTheme.fontFamily.sans],
       mono: ['"JetBrains Mono"', ...defaultTheme.fontFamily.mono],
-    },
-    extend: {
-      typography: (theme) => ({
-        gray: {
-          css: {
-            '--tw-prose-body': theme('colors.gray.900'),
-          },
-        },
-      }),
     },
   },
   variants: {},
